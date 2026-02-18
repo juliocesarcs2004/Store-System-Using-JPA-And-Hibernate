5f0f36165e65d1686b93257be5a0b1cfc98024a6
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2026-02-18 16:28:12 -0300

    Implemented methods for customized queries

--- loja/src/main/java/br/com/alura/loja/dao/ProdutodDao.java
+++ loja/src/main/java/br/com/alura/loja/dao/ProdutodDao.java
@@ -1,9 +1,9 @@
 package br.com.alura.loja.dao;
 
-import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
 import java.util.List;
 
 public class ProdutodDao {
@@ -34,6 +34,27 @@ public class ProdutodDao {
     public List<Produto> buscarTodos() {
         String jpql = "SELECT p FROM Produto p";
         return em.createQuery(jpql, Produto.class).getResultList();
+    }
+
+    public List<Produto> buscarPorNome(String nome) {
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public List<Produto> buscarPorNomeDaCategoria(String nome) {
+        String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public BigDecimal buscarPrecoDoProdutoComNome(String nome) {
+        String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, BigDecimal.class)
+                .setParameter("nome", nome)
+                .getSingleResult();
     }
 
 }
