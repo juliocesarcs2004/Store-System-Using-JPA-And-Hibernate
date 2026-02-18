5ecd67c2e487bf22f5df708a1f99fda012ffa308
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2026-02-18 11:54:35 -0300

    Modified product Class

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,22 +1,19 @@
 package br.com.alura.loja.testes;
 
 import br.com.alura.loja.dao.ProdutodDao;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.math.BigDecimal;
 
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
 
-        Produto celular = new Produto();
-        celular.setNome("Xiaomi Redmi");
-        celular.setDescricao("Muito Legal");
-        celular.setPreco(new BigDecimal("800"));
+        Produto celular = new Produto("Xiaomi Redmi",
+                "Muito Legal",new BigDecimal("800"), Categoria.CELULARES  );
 
         EntityManager em = JPAUtil.getEntityManager();
         ProdutodDao dao = new ProdutodDao(em);
