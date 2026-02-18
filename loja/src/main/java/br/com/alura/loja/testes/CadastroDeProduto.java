735151ff915a20869c700597e289d803bac93793
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2026-02-18 12:56:13 -0300

    Added Category Entity

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.testes;
 
+import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ProdutodDao;
 import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
@@ -12,14 +13,19 @@ public class CadastroDeProduto {
 
     public static void main(String[] args) {
 
+        Categoria celulares = new Categoria("CELULARES");
         Produto celular = new Produto("Xiaomi Redmi",
-                "Muito Legal",new BigDecimal("800"), Categoria.CELULARES  );
+                "Muito Legal",new BigDecimal("800"), celulares  );
 
         EntityManager em = JPAUtil.getEntityManager();
-        ProdutodDao dao = new ProdutodDao(em);
+        ProdutodDao produtoDao = new ProdutodDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
 
         em.getTransaction().begin();
-        dao.cadastrar(celular);
+
+        categoriaDao.cadastrar(celulares);
+        produtoDao.cadastrar(celular);
+
         em.getTransaction().commit();
         em.close();
     }
