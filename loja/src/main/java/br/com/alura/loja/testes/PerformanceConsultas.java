29aee6128a5e7291c0a1be1d057f478b175800bf
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2026-02-24 12:25:42 -0300

    Used JOIN FETCH to solve exception due to Lazy initialization

--- loja/src/main/java/br/com/alura/loja/testes/PerformanceConsultas.java
+++ loja/src/main/java/br/com/alura/loja/testes/PerformanceConsultas.java
@@ -15,8 +15,10 @@ public class PerformanceConsultas {
     public static void main(String[] args) {
         popularBancoDeDados();
         EntityManager em = JPAUtil.getEntityManager();
-        Pedido pedido = em.find(Pedido.class, 1L);
-        System.out.println(pedido.getItens().size());
+        PedidoDao pedidoDao = new PedidoDao(em);
+        Pedido pedido = pedidoDao.buscarPedidoComCliente(1L);
+        em.close();
+        System.out.println(pedido.getCliente().getNome());
     }
 
     private static void popularBancoDeDados() {
